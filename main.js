b146f36b3df5caa9e63db414f5ddbd4f97db3e6b
lggerman <lopez.ggerman@gmail.com>
2021-02-23 23:12:33 -0300

    cambios a la sección musica para acercarse a la apariencia deseada

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+/* eslint-disable linebreak-style */
 const $burgerButton = document.querySelector('.menu-burger');
-const $navBar= document.querySelector('.nav-bar')
+const $navBar = document.querySelector('.nav-bar');
 
-$burgerButton.addEventListener('click', () =>{
-    $burgerButton.classList.toggle('open');
-    $navBar.classList.toggle('active')
-})
+$burgerButton.addEventListener('click', () => {
+  $burgerButton.classList.toggle('open');
+  $navBar.classList.toggle('active');
+});
+const $divSongs = document.querySelectorAll('.div-songs');
+console.log($divSongs);
+
+$divSongs.forEach((div) => {
+  div.addEventListener('mouseover', () => {
+    div.childNodes[1].classList.toggle('opacity-active');
+    div.childNodes[3].classList.toggle('listen-active');
+    div.childNodes[5].classList.toggle('listen-active');
+  });
+});
+$divSongs.forEach((div) => {
+  div.addEventListener('mouseout', () => {
+    div.childNodes[1].classList.toggle('opacity-active');
+    div.childNodes[3].classList.toggle('listen-active');
+    div.childNodes[5].classList.toggle('listen-active');
+  });
+});
