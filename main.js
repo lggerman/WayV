26a888c69861d87a3d1c00ffd3a9dea88390175f
lggerman <lopez.ggerman@gmail.com>
2021-02-24 20:53:26 -0300

    posicionamiento de los elementos en la seccion music

--- main.js
+++ main.js
@@ -6,20 +6,50 @@ $burgerButton.addEventListener('click', () => {
   $burgerButton.classList.toggle('open');
   $navBar.classList.toggle('active');
 });
-const $divSongs = document.querySelectorAll('.div-songs');
-console.log($divSongs);
 
-$divSongs.forEach((div) => {
-  div.addEventListener('mouseover', () => {
-    div.childNodes[1].classList.toggle('opacity-active');
-    div.childNodes[3].classList.toggle('listen-active');
-    div.childNodes[5].classList.toggle('listen-active');
+// MediaQueries
+//
+//
+const $desktop = window.matchMedia('(min-width: 1024px)');
+if ($desktop.matches === true) {
+  const $divSongs = document.querySelectorAll('.div-songs');
+
+  $divSongs.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+      div.childNodes[1].classList.toggle('opacity-active');
+      div.childNodes[1].classList.toggle('listen-active-grayscale');
+      div.childNodes[3].classList.toggle('listen-active');
+      div.childNodes[5].classList.toggle('listen-active');
+    });
   });
-});
-$divSongs.forEach((div) => {
-  div.addEventListener('mouseout', () => {
-    div.childNodes[1].classList.toggle('opacity-active');
-    div.childNodes[3].classList.toggle('listen-active');
-    div.childNodes[5].classList.toggle('listen-active');
+  $divSongs.forEach((div) => {
+    div.addEventListener('mouseout', () => {
+      div.childNodes[1].classList.toggle('opacity-active');
+      div.childNodes[1].classList.toggle('listen-active-grayscale');
+      div.childNodes[3].classList.toggle('listen-active');
+      div.childNodes[5].classList.toggle('listen-active');
+    });
   });
-});
+}
+
+const $mobile = window.matchMedia('(min-width: 320px)');
+if ($mobile.matches === true) {
+  const $divSongs = document.querySelectorAll('.div-songs');
+
+  $divSongs.forEach((div) => {
+    div.addEventListener('touchend', () => {
+      div.childNodes[1].classList.toggle('opacity-active');
+      div.childNodes[1].classList.toggle('listen-active-grayscale');
+      div.childNodes[3].classList.toggle('listen-active');
+      div.childNodes[5].classList.toggle('listen-active');
+    });
+  });
+  /* $divSongs.forEach((div) => {
+    div.addEventListener('touch', () => {
+      div.childNodes[1].classList.toggle('opacity-active');
+      div.childNodes[1].classList.toggle('listen-active-grayscale');
+      div.childNodes[3].classList.toggle('listen-active');
+      div.childNodes[5].classList.toggle('listen-active');
+    });
+  }); */
+}
